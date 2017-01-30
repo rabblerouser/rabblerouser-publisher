0b29e93a1c2fc6f770ca5b9635e3ec571b3c103b
Cam Jackson <camjackson89@gmail.com>
2017-01-30 15:19:32 +1100

    Remove consumer code

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 //TODO: Design this so different streams technologies can be plugged in at some point(like nodemailer).
 
 const producer = require('./aws/producer');
-const consumer = require('./aws/consumer');
 
-module.exports = { producer, consumer };
+module.exports = { producer };
